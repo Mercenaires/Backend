b829581e6a559eb2741e9c29af5a1ae8a642d296
Ephraïm Kabangu <40006741@parisnanterre.fr>
2024-10-09 16:35:31 +0200

    Create HelloControllerTest.java

---
+++ app/src/test/java/org/example/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.example.controllers.HelloController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello from API"));
+    }
+}
