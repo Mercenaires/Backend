c187797a8ca56cdf0fd185d574a9561bdef02fbb
Ephraïm Kabangu <40006741@parisnanterre.fr>
2024-10-09 17:10:10 +0200

    Update HelloControllerTest.java

--- app/src/test/java/org/example/HelloControllerTest.java
+++ app/src/test/java/org/example/HelloControllerTest.java
@@ -1,24 +1,11 @@
 package org.example.controllers;
 
-import org.example.controllers.HelloController;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void testGetHello() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello from API"));
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello from API";
     }
 }
