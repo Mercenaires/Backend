084436809998b254b344d011ca1438c0e2faeeee
Ephraïm Kabangu <40006741@parisnanterre.fr>
2024-11-01 17:52:38 +0100

    Ajout de la classe YouTubeResponse pour gérer l'API

---
+++ app/src/main/java/org/example/models/YouTubeResponse.java
@@ -0,0 +1,39 @@
+package org.example.models;
+
+import java.util.List;
+
+public class YouTubeResponse {
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public static class Item {
+        private Snippet snippet;
+
+        public Snippet getSnippet() {
+            return snippet;
+        }
+
+        public void setSnippet(Snippet snippet) {
+            this.snippet = snippet;
+        }
+    }
+
+    public static class Snippet {
+        private String title;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
+}
